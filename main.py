670844b2b1fd91f4ff612251b926ad6087a4cec6
dead inside <90174775+xxxvvvxxxvx@users.noreply.github.com>
2021-11-15 07:19:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+'''
+15.11
+'''
+class Transportlidzeklis:
+
+def __init__(self, nosaukums, max_atrums, nobraukums):
+  self.nosaukums = Mercedes
+  self.max_atrums = 150
+  self.nobraukums = 55
+
+modelisX = Transportlidzeklis(“Mersedes”,150,55)
+print(modelisX.nosaukums, modelisX.max_atrums, modelisX.nobraukums)
+
+class Buss(Transportlidzeklis):
+  pass
+
+modelisX = Buss(“Mersedes”,150,55)
+print(modelisX.nosaukums, modelisX.max_atrums, modelisX.nobraukums)
+
+def sedvietu_skaits(self, skaits):
+  self.skaits = 50
+  return f"Sēdvietu skaits {self.nosaukums} ir {self.skaits} vietas."
+
+def sedvietu_skaits(self, skaits = 50):
+  return super().sedvietu_skaits(skaits = 50)
+
+Skolas_buss = Buss(“Mersedes”, 140, 20)
+print(Skolas_buss.sedvietu_skaits())
+
+krasa = "Melna"
+
+Skolas_buss = Buss(“Mersedes”, 140, 20)
+print(Skolas_buss.krasa, Skolas_buss.nosaukums, “Ātrums:”, Skolas_buss.max_atrums,
+“Nobraukums:”, Skolas_buss.nobraukums)
+
+class biletes:
+
+self.skaits * 0.5
+
+Skolas_buss = Buss(“Mersedes”)
+print(Skolas_buss.biletes())
